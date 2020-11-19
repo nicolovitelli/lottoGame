8eb748315dc6d9753d7fa4ba78e9ceda402cd4b5
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-11-19 12:04:35 +0100

    updated - added generatedTicket function

--- lotto/ticket.py
+++ lotto/ticket.py
@@ -1,5 +1,5 @@
-from wheel import Wheel
-import argparse
+from lotto.city import City
+import random
 
 class Ticket:
     def __init__(self, amount, type, numbers, city):
@@ -8,24 +8,32 @@ class Ticket:
         self.numbers = numbers
         self.city = city
 
-
     def printTicket(self):
         print("Number of Tickets: {}".format(self.amount))
         a = ["FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH"]
         types = {1: "Ambata", 2: "Ambo", 3: "Terna", 4: "Quaterna", 5: "Cinquina"}
         i = 0
         x = 0
-
         while i != self.amount:
             while x != len(self.type):
                 while x != len(self.numbers):
                     while x != len(self.city):
-                        print("----------------")
                         print("{} TICKET".format(a[i]))
                         print("Type: {}".format(types[self.type[x]]))
                         print("Numbers to generate: {}".format(self.numbers[x]))
-                        wheel = Wheel(self.numbers[x])
-                        wheel.randomWheel()
+                        ticket = Ticket(self.amount, self.type, self.numbers[x], self.city)
+                        ticket.generateTicket(self.numbers)
                         print("City: {}".format(self.city[x]))
+                        print("-----------------")
                         x = x + 1
                         i = i + 1
+
+    def generateTicket(self, numbers):
+        generated = []
+        x = 0
+        print("Numbers Generated:")
+        while x != self.numbers:
+            generated.append(random.randint(1, 90))
+            x = x + 1
+        for element in generated:
+            print("{}".format(element))
