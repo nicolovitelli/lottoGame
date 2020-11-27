bdd2e60884bb816191aae2ae33986c33fba22633
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-11-27 11:30:21 +0100

    updated the code

--- lotto/city.py
+++ lotto/city.py
@@ -1,20 +1,30 @@
 class City:
-    def __init__(self, city, cities_list, user_cities):
-        self.city = city
-        self.cities_list = cities_list
-        self.user_cities = user_cities
 
-    def pickCity(self):
-        for element in range(len(self.cities_list)):
-            if self.city == self.cities_list[element]:
-                self.user_cities.append(self.city)
-                return True
-            elif self.city == self.cities_list[element].lower():
-                self.user_cities.append(self.city)
-                return True
-            elif self.city == self.cities_list[element].upper():
-                self.user_cities.append(self.city)
-                return True
+    def __init__(self, city_number):
+        city_list = ["Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino",
+                     "Venezia", "Tutte"]
+        self.user_city = city_list[city_number - 1]
+
+    # this static method permits the user to choose a city by inserting its number
+    @staticmethod
+    def chooseCity():
+        city_list = ["Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino",
+                     "Venezia", "Tutte"]
+        City.printCities()
+        city_number = input("Insert the City (number required):  ")
+        while True:
+            # check if the number inserted by the user is valid
+            if 1 <= int(city_number) <= len(city_list):
+                return int(city_number)
+            # if not, the user needs to reinsert the number
             else:
-                pass
+                city_number = input("Invalid Number. Please choose a Number between 1 and 11: ")
 
+    # this static method shows the user all cities
+    @staticmethod
+    def printCities():
+        city_list = ["Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino",
+                     "Venezia", "Tutte"]
+        # printing all elements in city_list
+        for i in range(len(city_list)):
+            print("{} - {}".format(i + 1, city_list[i]))
