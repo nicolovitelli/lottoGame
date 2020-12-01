0154f9e2c5d9d7c38e0e3c29e5cdafcfe733eb20
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-12-01 13:46:16 +0100

    updated

--- lotto/bet.py
+++ lotto/bet.py
@@ -1,18 +1,17 @@
 class Bet:
+    ticket_types = ["Ambata", "Ambo", "Terna", "Quaterna", "Cinquina"]
 
     def __init__(self, ticket_type_number):
-        ticket_types = ["Ambata", "Ambo", "Terna", "Quaterna", "Cinquina"]
-        self.user_ticket_type = ticket_types[ticket_type_number]
+        self.user_ticket_type = Bet.ticket_types[ticket_type_number]
 
     # this static method permits the user to insert the bet type
     @staticmethod
     def betType():
-        ticket_types = ["Ambata", "Ambo", "Terna", "Quaterna", "Cinquina"]
         Bet.printTicketTypes()
         ticket_type = input("Insert the Ticket type (number required): ")
         while True:
             # check if the number inserted by the user is valid or not
-            if 1 <= int(ticket_type) <= len(ticket_types):
+            if 1 <= int(ticket_type) <= len(Bet.ticket_types):
                 return int(ticket_type)
             # if not, the user must reinsert a valid number
             else:
@@ -21,7 +20,6 @@ class Bet:
     # this static method shows the user all ticket types
     @staticmethod
     def printTicketTypes():
-        ticket_types = ["Ambata", "Ambo", "Terna", "Quaterna", "Cinquina"]
         print("\nHere's the Ticket Types:")
-        for i in range(len(ticket_types)):
-            print("{} - {}".format(i + 1, ticket_types[i]))
+        for i in range(len(Bet.ticket_types)):
+            print("{} - {}".format(i + 1, Bet.ticket_types[i]))
