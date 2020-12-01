e3c19750ef1fe82fe9a4cae96c6c47a01187dfff
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-12-01 13:45:34 +0100

    removed generateTicket function
    now numbers can be generated with its class NumbersGenerator

--- lotto/ticket.py
+++ lotto/ticket.py
@@ -1,22 +1,13 @@
 from lotto.city import City
 from lotto.bet import Bet
-import random
+from lotto.numbersgenerator import NumbersGenerator
 
 
 class Ticket:
-    def __init__(self, city_number, bet_type, tickets_number):
+    def __init__(self, city_number, bet_type, num_gen):
         self.city = City(city_number)
         self.bet_type = Bet(bet_type)
-        self.generated_numbers = []
-        self.generateTicket(tickets_number)
-
-    """ this little function will generate the numbers from 1 to 90 and insert the results into the generated_numbers
-    array """
-    def generateTicket(self, tickets_number):
-        while len(self.generated_numbers) < tickets_number:
-            number = random.randint(1, 90)
-            if number not in self.generated_numbers:
-                self.generated_numbers.append(str(number))
+        self.generated_numbers = NumbersGenerator(num_gen)
 
     """ this function shows the ticket to the user """
     def printTicket(self):
@@ -26,6 +17,6 @@ class Ticket:
         print("|    City: {}    ".format(self.city.user_city.upper()))
         print("|    Ticket Type: {}    ".format(self.bet_type.user_ticket_type.upper()))
         # printing all generated numbers
-        for number in self.generated_numbers:
+        for number in self.generated_numbers.generated_numbers:
             print("     {} ".format(number), end="")
         print("\n")
