26c620bc4953664fc3bda5143631f4bb5537f166
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-12-01 13:44:19 +0100

    added results()

--- lotto/extraction.py
+++ lotto/extraction.py
@@ -16,3 +16,33 @@ class Extraction:
     def printExtraction(self):
         for city in self.table.keys():
             print("{} - {}".format(city, self.table[city].generated_numbers))
+
+    def results(self, tickets):
+        winning_ticket = False
+        for ticket in tickets:
+            if ticket.city.user_city.lower() == 'tutte':
+                win_dict = {}
+                for city in self.table.keys():
+                    win_list = []
+                    for number in ticket.generated_numbers.generated_numbers:
+                        if number in self.table[city].generated_numbers:
+                            win_list.append(number)
+                    win_dict[city] = win_list
+            else:
+                win_dict = {}
+                win_list = []
+                for number in ticket.generated_numbers.generated_numbers:
+                    if number in self.table[ticket.city.user_city].generated_numbers:
+                        win_list.append(number)
+                win_dict[ticket.city.user_city] = win_list
+            for city in win_dict.keys():
+                if len(win_dict[city]) > Bet.ticket_types.index(ticket.bet_type.user_ticket_type):
+                    print("-- RESULTS --")
+                    print("You Win :)")
+                    print("Your Winning Ticket: ")
+                    ticket.printTicket()
+                    print("Winning City: {}".format(city))
+                    winning_ticket = True
+        if not winning_ticket:
+            print("-- RESULTS --")
+            print("You Lose :(")
