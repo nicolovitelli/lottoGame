7c8c1d1b2085c1387cc8952925f58689f4012c5a
Nicolò Vitelli <58621823+nicolovitelli@users.noreply.github.com>
2020-12-02 14:18:54 +0100

    table -> numbers_extracted_each_city

--- lotto/extraction.py
+++ lotto/extraction.py
@@ -5,34 +5,34 @@ from lotto.numbersgenerator import NumbersGenerator
 
 class Extraction:
     def __init__(self):
-        self.table = {}
+        self.numbers_extracted_each_city = {}
         self.extraction()
 
     def extraction(self):
         for city in City.city_list:
-            self.table[city] = NumbersGenerator(5)
+            self.numbers_extracted_each_city[city] = NumbersGenerator(5)
 
     # this function will print all numbers (from self.table) for each city in city_list
     def printExtraction(self):
-        for city in self.table.keys():
-            print("{} - {}".format(city, self.table[city].generated_numbers))
+        for city in self.numbers_extracted_each_city.keys():
+            print("{} - {}".format(city, self.numbers_extracted_each_city[city].generated_numbers))
 
     def results(self, tickets):
         winning_ticket = False
         for ticket in tickets:
             if ticket.city.user_city.lower() == 'tutte':
                 win_dict = {}
-                for city in self.table.keys():
+                for city in self.numbers_extracted_each_city.keys():
                     win_list = []
                     for number in ticket.generated_numbers.generated_numbers:
-                        if number in self.table[city].generated_numbers:
+                        if number in self.numbers_extracted_each_city[city].generated_numbers:
                             win_list.append(number)
                     win_dict[city] = win_list
             else:
                 win_dict = {}
                 win_list = []
                 for number in ticket.generated_numbers.generated_numbers:
-                    if number in self.table[ticket.city.user_city].generated_numbers:
+                    if number in self.numbers_extracted_each_citye[ticket.city.user_city].generated_numbers:
                         win_list.append(number)
                 win_dict[ticket.city.user_city] = win_list
             for city in win_dict.keys():
